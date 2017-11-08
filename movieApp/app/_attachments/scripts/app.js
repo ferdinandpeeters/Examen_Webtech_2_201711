fe5207675e8a9dc95eea995d3878bc4e43ed9189
FFLP <ferdi_9678@hotmail.com>
2017-11-08 16:01:02 +0100

    ophalen en tonen van films

--- movieApp/app/_attachments/scripts/app.js
+++ movieApp/app/_attachments/scripts/app.js
@@ -12,13 +12,19 @@ angular.module('movieApp', ['ngRoute'])
 	
 	.controller('homeCtrl', function($scope, personSrv, zoneSrv, saveSrv) {
 		
-	    	$('searchButton').on('click', function (e) {
+	    	$('#searchButton').on('click', function (e) {
 
 	    		$scope.movies = '';
 
-	    		var person = $('#personText').val();
+	    		var name = $('#nameText').val();
 	    		
-	    		personSrv.getCoordinates(address).then(function(data){
+	    		personSrv.getPerson(name).then(function(data){
+	    			var arrayMovies = [];
+	    			for (var i = 0; i < data.data[0].filmography.actor.length; i++) { 
+	    				arrayMovies.push(data.data[0].filmography.actor[i].title);
+	    			}
+	    			$scope.movies = arrayMovies;
+	    			/*
 	    			var lat = parseFloat(data.data[0].lat);
 	    			var lon = parseFloat(data.data[0].lon);
 		    		var zones = saveSrv.getObject('zones');
@@ -33,14 +39,15 @@ angular.module('movieApp', ['ngRoute'])
 		    		else {
 		    			$scope.color = zoneSrv.getTariff(lon, lat, zones.data);
 		    		}
+		    		*/
 	    		});
 	    	});
     })
    
     .service('personSrv', function($http, $q) {
-    		this.getPerson = function(address) {
+    		this.getPerson = function(name) {
 	    		var q = $q.defer();
-	    		var url = 'http://nominatim.openstreetmap.org/search?q=' + encodeURIComponent(address) + '&format=json';
+	    		var url = 'http://theimdbapi.org/api/find/person?name=' + encodeURIComponent(name) + '&format=json';
 
 	    		$http.get(url)
 	    			.then(function(data){
@@ -64,56 +71,6 @@ angular.module('movieApp', ['ngRoute'])
 				});
 			
 			return q.promise;
-		};
-		
-		// http://alienryderflex.com/polygon/
-		this.inPolygon = function(location, polyLoc){
-			var lastPoint = polyLoc[polyLoc.length-1];
-			var isInside = false;
-			var x = location[0];
-
-			for(var i = 0; i < polyLoc.length; i++){
-				var point = polyLoc[i];
-				var x1 = lastPoint[0];
-				var x2 = point[0];
-				var dx = x2 - x1;
-
-				if(Math.abs(dx) > 180.0){
-					if(x > 0){
-						while(x1 < 0)
-							x1 += 360;
-						while(x2 < 0)
-							x2 += 360;
-					}
-					else{
-						while(x1 > 0)
-							x1 -= 360;
-						while(x2 > 0)
-							x2 -= 360;
-					}
-					dx = x2 - x1;
-				}
-
-				if((x1 <= x && x2 > x) || (x1 >= x && x2 < x)){
-					var grad = (point[1] - lastPoint[1]) / dx;
-					var intersectAtLat = lastPoint[1] + ((x - x1) * grad);
-
-					if(intersectAtLat > location[1])
-						isInside = !isInside;
-				}
-				lastPoint = point;
-			}
-			return isInside;
-		};
-		
-		this.getTariff = function(lng, lat, zones){
-			for(var i = 0; i < zones.length; i++) {
-				var geo = JSON.parse(zones[i].geometry);
-				var coordinates = geo.coordinates[0];
-				if(this.inPolygon([lng, lat], coordinates)) {
-					return zones[i].tariefkleur;
-				}
-			}
 		};
     })
     
